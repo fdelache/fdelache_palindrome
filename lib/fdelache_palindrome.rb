58f6210069d0a11d63669d2a21bd5c4050bc652a
Franck Delache <franck.delache@shopify.com>
2018-11-20 09:23:39 -0500

    Finished implementation of palindrome method
    With testing as well.

--- lib/fdelache_palindrome.rb
+++ lib/fdelache_palindrome.rb
@@ -1,5 +1,16 @@
 require "fdelache_palindrome/version"
 
-module FdelachePalindrome
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    scan(/[a-z]/i).join.downcase
+  end
 end
